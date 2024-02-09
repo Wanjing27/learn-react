5c55d3fc4934a52794628542cbf5ba14b42569f3
wanjing27 <wanjing0227@gmail.com>
2024-02-09 01:56:57 -0500

    02/07/2024

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,27 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Gallery from './components/gallery'
+import Profile1 from './qcomps/profile_mistake'
+import Profile2 from './qcomps/firstcomp'
+import Bios from './qcomps/bios'
+import Todos from './qcomps/todos'
+import Props from './qcomps/gallery_props'
+import Avatat from './components/profile_props'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <Gallery/>
+        <Profile1/>
+        <Profile2/>
+        <Bios/>
+        <Todos/>
+        <Avatat/>
+        <Props/>
     </div>
+
+
   )
 }
